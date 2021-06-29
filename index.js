f156bb1b1d2f01a670c4981baa5175cc25492b9e
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-29 10:45:20 +0530

    first files to be commited

---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+const express = require('express')
+const {google}  = require('googleapis')
+
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_ID
+const REDIRECT_URI = process.env.REDIRECT_URI
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN
+
+const oAuthClient = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI)
+oAuthClient.setCredentials({refresh_token:REFRESH_TOKEN})
+const port = process.env.PORT||8000
+const app = express()
+app.use(express.json())
+
+app.get('/',(req,response)=>{
+    
+    
+    async function sendMail(){
+        try {
+            const accessToken = await oAuthClient.getAccessToken()
+    
+            const transport  = nodemailer.createTransport({
+                service:'gmail',
+                auth:{
+                    type:'OAuth2',
+                    user:'kowshikerappajipatel@gmail.com',
+                    clientId:CLIENT_ID,
+                    clientSecret:CLIENT_SECRET,
+                    refreshToken:REFRESH_TOKEN,
+                    accessToken:accessToken
+                }
+               
+            })
+            const mailOptions = {
+                from:'<kowshikerappajipatel@gmail.com>',
+                to:'kowshiksepatel@gmail.com',
+                subject:'Hello from gmail using API',
+                text:'Hello from gmail using API',
+                html:'<h1>Hello from gmail using API</h1>'
+    
+            }
+    
+            const result  = await transport.sendMail(mailOptions)
+    
+            return result
+    
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    sendMail()
+    .then(res =>{
+        console.log('Email sent....',res)
+        response.status(200).json({res})
+    })
+    .catch(err => {
+        console.log(err)
+        response.status(500).json({err})
+    })
+
+})
+
+
+app.listen(port,()=>{console.log('server started with port ',port)} )
