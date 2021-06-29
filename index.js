ac98b6ce0ee8b061fa436065162152e95154ff24
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-29 10:56:17 +0530

    debugged files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const {google}  = require('googleapis')
 
 const CLIENT_ID = process.env.CLIENT_ID
-const CLIENT_SECRET = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URI = process.env.REDIRECT_URI
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN
 
